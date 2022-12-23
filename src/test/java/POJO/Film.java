714563c0058a304a8b447137dced2cc99cc27e83
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-23 18:08:48 +0300

    Добавлены POJO классы из JSON

---
+++ src/test/java/POJO/Film.java
@@ -0,0 +1,15 @@
+package POJO;
+
+import java.util.ArrayList;
+
+public class Film {
+    public String name;
+    public Integer season;
+    public Integer series;
+    public String year;
+    public ArrayList<String> genres;
+    public String country;
+    public ArrayList<Crew> crew;
+    public ArrayList<Actor> actors;
+    public String premierDate;
+}
