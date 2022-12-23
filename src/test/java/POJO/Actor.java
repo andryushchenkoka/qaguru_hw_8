00cefe38477428785933a0a0d423b89cdaaea85e
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-23 19:20:32 +0300

    Тесты для json

--- src/test/java/POJO/Actor.java
+++ src/test/java/POJO/Actor.java
@@ -3,4 +3,12 @@ package POJO;
 public class Actor {
     public String name;
     public String character;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
 }
--- src/test/java/tests/FileTest.java
+++ src/test/java/tests/FileTest.java
@@ -1,16 +1,22 @@
 package tests;
 
+import POJO.Film;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import parsing.FileParser;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileTest {
     FileParser fileParser = new FileParser("qaguru_hw_8.zip");
+    String jsonFile = "Film.json";
+    ClassLoader classLoader = FileTest.class.getClassLoader();
 
     @Test
-    public void verifyPdfTest(){
+    public void verifyPdfTest() {
         String pdfContent = fileParser.readPdfFromZip(true);
 
         assertTrue(pdfContent.contains("Мой дядя"));
@@ -19,16 +25,26 @@ public class FileTest {
     }
 
     @Test
-    public void verifyXlsxTest(){
+    public void verifyXlsxTest() {
         String cellContent = fileParser.readXlsxFromZip(0, 2, 0, true);
 
         assertEquals("Щелкунчик", cellContent);
     }
 
     @Test
-    public void verifyCsvTest(){
-        String csvContent = fileParser.readCsvFromZip(4, 0,true);
+    public void verifyCsvTest() {
+        String csvContent = fileParser.readCsvFromZip(4, 0, true);
 
         assertEquals("Тихий Дон", csvContent);
+    }
+
+    @Test
+    public void jsonTest() throws IOException {
+        Film film = new ObjectMapper().readValue(classLoader.getResourceAsStream(jsonFile), Film.class);
+
+        assertEquals("Wednesday", film.getName());
+        assertEquals("crime", film.getGenres().get(2));
+        assertEquals("Tim Burton", film.getCrew().get(0).getName());
+        assertEquals("Enid Sinclair", film.getActors().get(1).getCharacter());
     }
 }
