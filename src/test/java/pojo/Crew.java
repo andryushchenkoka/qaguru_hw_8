8982cbbf88c540302f24db10eca63480ed224cf8
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-24 20:01:48 +0300

    Пакет POJO переименован в pojo

--- src/test/java/pojo/Crew.java
+++ src/test/java/pojo/Crew.java
@@ -1,4 +1,4 @@
-package POJO;
+package pojo;
 
 public class Crew {
     public String name;
--- src/test/java/tests/FileTest.java
+++ src/test/java/tests/FileTest.java
@@ -1,6 +1,6 @@
 package tests;
 
-import POJO.Film;
+import pojo.Film;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import parsing.FileParser;
